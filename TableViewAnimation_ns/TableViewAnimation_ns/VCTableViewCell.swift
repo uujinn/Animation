830c8893210d61802c406842ff36f358bbb3e663
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-22 17:22:40 +0900

    [feat] moving cell Animation 구현

--- TableViewAnimation_ns/TableViewAnimation_ns/VCTableViewCell.swift
+++ TableViewAnimation_ns/TableViewAnimation_ns/VCTableViewCell.swift
@@ -6,20 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 class VCTableViewCell: UITableViewCell {
     
     static let identifier = "VCTableViewCell"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    let img: UIImageView = {
+        let imgView = UIImageView()
+        imgView.image = UIImage(named: "Image")
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imgView
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "1st"
+        label.textColor = UIColor.gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addContentView()
+        autoLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
     }
-
+    
+    private func addContentView(){
+        contentView.addSubview(img)
+        contentView.addSubview(label)
+//        self.contentView.backgroundColor = .purple
+    }
+    private func autoLayout(){
+        img.snp.makeConstraints{
+            $0.centerY.equalToSuperview()
+            $0.size.width.height.equalTo(75)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        label.snp.makeConstraints{
+            $0.leading.equalTo(img.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
