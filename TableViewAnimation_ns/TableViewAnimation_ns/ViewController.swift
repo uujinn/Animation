05c146992a4be018ff7bf4b3b85fb370b9473aec
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-22 15:07:42 +0900

    [FEAT] No-Storyboard Setting

---
+++ TableViewAnimation_ns/TableViewAnimation_ns/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TableViewAnimation_ns
+//
+//  Created by 양유진 on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
+
